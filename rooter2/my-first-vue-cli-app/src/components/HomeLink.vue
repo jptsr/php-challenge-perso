180464a0bf312a465dee06e805997adc51187c95
Julie <juptsr.becode@gmail.com>
2022-05-09 17:59:39 +0200

    first app with vue components & props - vue router added

---
+++ rooter2/my-first-vue-cli-app/src/components/HomeLink.vue
@@ -0,0 +1,15 @@
+<script>
+    export default ({
+        name: 'HomeLink'
+    })
+</script>
+
+<template>
+    <a href="/">Acceuil</a>
+</template>
+
+<style>
+    a {
+        text-decoration: none;
+    }
+</style>
---
+++ rooter2/my-first-vue-cli-app/src/components/NavLink.vue
@@ -0,0 +1,17 @@
+<script>
+    export default ({
+        name: 'NavLink',
+        props: ['url', 'text']
+    })
+</script>
+
+<template>
+    <a :href="url" class="nav-link"> {{ text }} </a>
+</template>
+
+<style>
+    .nav-link {
+        display: flex;
+        justify-content: center;
+    }
+</style>
